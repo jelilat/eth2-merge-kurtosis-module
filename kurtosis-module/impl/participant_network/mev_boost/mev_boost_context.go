47202616b1c15f3d014827c740ee7c06b488cd7f
Jelilat Anofiu <anofiujelailah@gmail.com>
2022-08-06 17:23:33 +0100

    add mev-boost context

--- kurtosis-module/impl/participant_network/mev_boost/mev_boost_context.go
+++ kurtosis-module/impl/participant_network/mev_boost/mev_boost_context.go
@@ -1,5 +1,45 @@
 package mev_boost
 
+// Still needs to be checked that it's collecting the right info
 type MEVBoostContext struct {
-	// TODO Information about the mev-boost node that the CL client needs, analogous to ELClientContext
+	// Information about the mev-boost node that the CL client needs, analogous to ELClientContext
+	clientName       string
+	enr              string // Ethereum Node Records
+	enode            string
+	ipAddr           string
+	rpcPortNum       uint16
+	wsPortNum        uint16
+	engineRpcPortNum uint16
+}
+
+func NewMEVBoostContext(clientName string, enr string, enode string, ipAddr string, rpcPortNum uint16, wsPortNum uint16, engineRpcPortNum uint16) *MEVBoostContext {
+	return &MEVBoostContext{clientName: clientName, enr: enr, enode: enode, ipAddr: ipAddr, rpcPortNum: rpcPortNum, wsPortNum: wsPortNum, engineRpcPortNum: engineRpcPortNum}
+}
+
+func (ctx *MEVBoostContext) GetClientName() string {
+	return ctx.clientName
+}
+
+func (ctx *MEVBoostContext) GetENR() string {
+	return ctx.enr
+}
+
+func (ctx *MEVBoostContext) GetEnode() string {
+	return ctx.enode
+}
+
+func (ctx *MEVBoostContext) GetIPAddress() string {
+	return ctx.ipAddr
+}
+
+func (ctx *MEVBoostContext) GetRPCPortNum() uint16 {
+	return ctx.rpcPortNum
+}
+
+func (ctx *MEVBoostContext) GetWSPortNum() uint16 {
+	return ctx.wsPortNum
+}
+
+func (ctx *MEVBoostContext) GetEngineRPCPortNum() uint16 {
+	return ctx.engineRpcPortNum
 }
